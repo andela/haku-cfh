daf62ebbb115d79b3b167cb53f2a8984ae564770
hector101 <hector1014really32@gmail.com>
2017-06-07 13:19:15 +0100

    chore(eslint-setup): add require-jsdocs rule to enforce  user to add a js-docs on class methods and functions
    Finishes #146683069

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,22 @@
 module.exports = {
     "extends": "airbnb-base",
     "plugins": [
-        "import"
+      "import"
     ],
     "env": {
-        "node": true,
-        "browser": true,
+      "node": true,
+      "browser": true,
+      "mocha": true,
+      "supertest": true,
     },
+    "rules": {
+       "require-jsdoc": ["error", {
+        "require": {
+            "FunctionDeclaration": true,
+            "MethodDefinition": true,
+            "ClassDeclaration": false,
+            "ArrowFunctionExpression": false
+        },
+    }],
+  }
 };
