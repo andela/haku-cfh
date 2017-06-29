399e1563f49b9178273ef5decad2f87e3d01c0dd
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-06-29 12:57:07 +0100

    feature(flight): install jsonwebtoken package
    this package include JWT functions
    
    Start #146683089

---
+++ app/controllers/auth.js
@@ -0,0 +1,39 @@
+var passport = require('passport');
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+var avatars = require('./avatars').all();
+
+exports.signup = function (req, res) {
+    "use strict";
+    if (!req.body.name || !req.body.email || !req.body.password) {
+        return res.redirect('/#!/signup?error=incomplete');
+    }
+
+    User.findOne({
+        email: req.body.email
+    }).exec(function (err, existingUser) {
+        if (err) {
+            return res.status(400).send('An Error Occurred');
+        }
+
+        if (!existingUser) {
+            var user = new User(req.body);
+            user.avatar = avatars[user.avatar];
+            user.provider = 'local';
+            user.save(function (err) {
+                if (err) {
+                    return res.render('/#!/signup?error=unknown', {
+                        errors: err.errors,
+                        user: user
+                    });
+                }
+
+                var token = user.generateToken();
+                res.status(201).send({ token: token });
+            });
+        } else {
+            return res.status(400).json({ message: 'User exists' });
+        }
+    });
+};
