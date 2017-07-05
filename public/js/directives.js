355255a157bca7d2213d9b3aab6f6522eff4ee25
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-07-05 12:02:11 +0100

    feature(authentication): user login
    a user receives a token on login.
    removed commented out code from directives.js
    
    Finishes #146683087

--- public/js/directives.js
+++ public/js/directives.js
@@ -71,10 +71,6 @@ angular.module('mean.directives', [])
       link: function(scope, elem, attr) {
         scope.showOptions = true;
 
-        // if (scope.$$childHead.global.authenticated === true) {
-        //   scope.showOptions = false;
-        // }
-
           if (localStorage.token) {
               scope.showOptions = false;
           }
