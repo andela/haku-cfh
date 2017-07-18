e5f1f3749d08eeae35ea3d2abfd07f4a6d5e5498
hector101 <hector1014really32@gmail.com>
2017-07-18 16:09:34 +0100

    feature(refactor-ui): refactor user interface

--- app/models/game.js
+++ app/models/game.js
@@ -5,10 +5,11 @@ const Schema = mongoose.Schema;
 const GameSchema = new Schema({
   gameId: { type: String, required: true },
   gameRound: { type: Number, default: 0 },
-  gameOwner: { type: String, required: true },
-  gameWinner: { type: String, required: true },
+  gameOwner: { type: Object, required: true },
+  gameWinner: { type: Object, required: true },
   gamePlayers: { type: Array, default: [] },
-  gameEnded: { type: Boolean, default: false }
+  gameEnded: { type: Boolean, default: false },
+  timePlayed: { type: Date, default: new Date().toUTCString() }
 });
 
 mongoose.model('Game', GameSchema);
